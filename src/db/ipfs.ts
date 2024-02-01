b92ed8011bc26149f7d0e819cc5c50d2fefbdb4b
boyroywax <jmsdevln@gmail.com>
2024-01-31 17:05:57 -0800

    refactor:progress: continue splitting prservices

--- src/db/ipfs.ts
+++ src/db/ipfs.ts
@@ -1,11 +1,13 @@
 import { MemoryBlockstore } from 'blockstore-core'
 import { MemoryDatastore } from 'datastore-core'
 import { Helia, createHelia } from 'helia'
+import { Libp2p } from 'libp2p'
 
-import { LibP2pNode } from './libp2pConfig';
-import { Libp2p } from 'libp2p/dist/src';
+import { Libp2pNode } from './libp2p.js'
 
 class IPFSNode {
+  public instance: Helia | null = null;
+
   public constructor() {
     const initialize = async () => {
       const ipfs = await this.create();
@@ -15,145 +17,22 @@ class IPFSNode {
     initialize();
   }
 
-  public heliaInitConfig: any = {
-
-
-  private async create(): Promise<{ ipfs: Helia }> {
+  private async create(): Promise<Helia> {
     const datastore = new MemoryDatastore()
     const blockstore = new MemoryBlockstore()
 
-    const libp2pNode: Libp2p = new LibP2pNode().instance as Libp2p
+    const libp2pNode: Libp2p = new Libp2pNode().instance as Libp2p
 
     const ipfs: Helia = await createHelia({
-      blockstore,
-      datastore,
-      libp2p: libp2pNode.instance
-    })
-
-    return ipfs
-  }
-
-
-
-}
-
-
-
-
-export class OrbitDBNode {
-
-  public orbitdbNode: any = null;
-  public helia: Helia | null = null;
-  public libp2pNode: Libp2p | null = null;
-  public enableDID: boolean | null = null;
-  public openDb: any = null;
-
-  public constructor(
-    {
-      databaseType = OrbitDBTypes.EventLog,
-      databaseName = 'orbitdb',
-      enableDID = false
-    }: OrbitDBNodeOptions = {
-      databaseType: OrbitDBTypes.EventLog,
-      databaseName: 'orbitdb',
-      enableDID: false
-    }
-  ) {
-    const initialize = async () => {
-      const { orbitdb, helia, libp2pNode } = await this.createNode({
-        enableDID
-      });
-      this.orbitdbNode = orbitdb;
-      this.helia = helia;
-      this.libp2pNode = libp2pNode;
-
-      if (databaseName !== null && databaseType !== null) {
-        const { db } = await this.startNode({
-          databaseName,
-          databaseType
-        });
-        this.openDb = db;
-      }
-    };
-
-    initialize();
-  }
-
-  public async createNode(
-    {
-      enableDID = false
-    }: OrbitDBDIDOptions = {
-      enableDID: false
-    }
-  ): Promise<{ orbitdb: any, helia: any, libp2pNode: any }> {
-    const datastore = new MemoryDatastore()
-    const blockstore = new MemoryBlockstore()
-
-    const libp2pNode = await createLibp2p(libp2pConfig)
-
-    const helia: Helia = await createHelia({
       blockstore,
       datastore,
       libp2p: libp2pNode
     })
 
-    let orbitdb: any = null
-
-    if (enableDID) {
-
-      const seed = new Uint8Array([157, 94, 116, 1918, 1339, 248, 93, 239, 173, 82, 245, 222, 199, 7, 183, 177, 123, 238, 83, 240, 143, 188, 87, 191, 33, 95, 58, 136, 46, 218, 219, 245])
-
-      OrbitDBIdentityProviderDID.setDIDResolver(KeyDidResolver.getResolver())
-      useIdentityProvider(OrbitDBIdentityProviderDID)
-      const didProvider = new Ed25519Provider(seed)
-      const provider = OrbitDBIdentityProviderDID({ didProvider })
-      orbitdb = await createOrbitDB({
-        ipfs: helia,
-        identity: { provider } 
-      })
-    }
-    else {
-      orbitdb = await createOrbitDB({
-        ipfs: helia
-      })
-    }
-    
-    return { orbitdb, helia, libp2pNode }
-    
-  }
-
-  public async startNode(
-    {
-      databaseName = 'orbitdb',
-      databaseType = OrbitDBTypes.EventLog
-    }: OrbitDBOptions = {
-      databaseName: 'orbitdb',
-      databaseType: OrbitDBTypes.EventLog
-    }
-  ): Promise<{ db: any }> {
-    const db = await this.orbitdbNode.open(databaseName, { type: databaseType })
-
-    const listenAddrs = this.libp2pNode?.getMultiaddrs()
-    console.log('libp2p is listening on the following addresses: ', listenAddrs)
-
-    console.log('OrbitDB address: ', db.address.toString())
-
-    // libp2pNode.addEventListener('peer:discovery', async (evt: any) => {
-    //   console.log('Discovered %s', evt.detail.id.toString()) // Log discovered peer
-    // })
-
-    // libp2pNode.addEventListener('peer:connect', (evt: any) => {
-    //   console.log('Connected to %s', evt.detail.toString()) // Log connected peer
-    // })
-
-    return { db }
-  }
-
-
-
-  public async closeDb() {
-    await this.openDb.close()
-    return 'Database closed'
+    return ipfs 
   }
 }
 
+export {
+  IPFSNode
+}
--- src/db/setup.ts
+++ src/db/setup.ts
@@ -1,4 +1,86 @@
+import { createOrbitDB, useIdentityProvider } from '@orbitdb/core'
+import OrbitDBIdentityProviderDID from '@orbitdb/identity-provider-did'
+import KeyDidResolver from 'key-did-resolver'
+import { Ed25519Provider } from 'key-did-provider-ed25519'
 
+import { IPFSNode } from "./ipfs.js"
+import { OrbitDBManifest, OrbitDBNodeOptions } from '../models/orbitdb.js'
+
+
+class OrbitDBInstance {
+  public ipfs: IPFSNode
+  public orbitdbOptions: OrbitDBNodeOptions
+  public instance: any = null
+
+  public constructor(ipfs: IPFSNode, orbitdbOptions: OrbitDBNodeOptions) {
+    this.ipfs = ipfs
+    this.orbitdbOptions = orbitdbOptions
+  }
+
+  private configNode() {
+    let config = {}
+    if (this.orbitdbOptions.enableDID) {
+      config = { identity: { provider: this.enableDID()} }
+    }
+    config['directory'] = this.createRandomDirectory()
+    return config
+    
+
+  public async createNode() {
+    
+    this.instance = await createOrbitDB(this.ipfs.instance, { })
+  }
+
+  private enableDID() {
+    const seed = new Uint8Array([157, 94, 116, 1918, 1339, 248, 93, 239, 173, 82, 245, 222, 199, 7, 183, 177, 123, 238, 83, 240, 143, 188, 87, 191, 33, 95, 58, 136, 46, 218, 219, 245])
+
+    OrbitDBIdentityProviderDID.setDIDResolver(KeyDidResolver.getResolver())
+    useIdentityProvider(OrbitDBIdentityProviderDID)
+    const didProvider = new Ed25519Provider(seed)
+    const provider = OrbitDBIdentityProviderDID({ didProvider })
+    return provider
+  }
+
+  private createRandomDirectory() {
+    return Math.random().toString(36).substring(7)
+  }
+
+  private async openDb() {
+
+  }
+
+
+
+}
+
+
+
+
+class OrbitDBSetup {
+  public constructor(ipfs: IPFSNode) {
+    this.ipfs = ipfs
+  }
+  
+  public async createNode() {
+    this.orbitdb = createOrbitDB(this.ipfs.instance, { })
+  }
+
+  public async openDb(options: any) {
+    const orbitdb = await this.orbitdb.open(
+      options.databaseName,
+      options.databaseType, 
+      {
+        ipfs: this.ipfs,
+        accessController: {
+          write: ['*']
+        }
+      }
+    )
+    return new OrbitDBActions(orbitdb)
+  }
+
+
+}
 
 class OrbitDBActions {
   private openDb: any
